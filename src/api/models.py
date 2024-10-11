f4e48d27d2ec30920962164056c9d76706830689
Daniel Perdomo <136826084+GoblinSwarm@users.noreply.github.com>
2024-10-11 01:12:21 +0000

    Backend Starting point working

--- src/api/models.py
+++ src/api/models.py
@@ -4,10 +4,10 @@ db = SQLAlchemy()
 
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=False, nullable=False)
+    name = db.Column(db.String(250), unique=False, nullable=False)
     birthday = db.Column(db.Date, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), unique=False, nullable=False)
     salt = db.Column(db.String(200), nullable=False)
 
     team_id = db.Column(db.Integer, db.ForeignKey('team.team_id'), nullable=False)
